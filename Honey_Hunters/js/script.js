0ffc75f9f756fd168ca0c000484af2b06df52f08
SeRVaIK <ovticalint@gmail.com>
2021-10-20 20:51:00 +0500

    1

--- Honey_Hunters/js/script.js
+++ Honey_Hunters/js/script.js
@@ -1,5 +1,3 @@
-// col-xl-4 col-lg-5 col-md-5 col-sm-12 
-
 "use strict"
 
 // Стандартная проверка того, что документ уже загружен
@@ -7,47 +5,54 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
+    function formSend(e) {
         e.preventDefault();
-        // let error = 0;
+
         let error = formValidate(form);
 
-        let formData = new FormData(form);
 
         if (error === 0) {
 
-            form.classList.add('_sending');
+            let cardCreation = document.querySelector('.cards__container');
 
-            let response = await fetch('./php/index.php', {
-                method: 'POST',
-                body: formData
-            });
-
-            if (response.ok) {
-                let result = await response.json();
-                form.reset();
-                form.classList.remove('_sending');
-                getResponce();
-            } else {
-                alert("Ошибка в передаче данных!");
-                form.classList.remove('_sending');
-            }
+            cardCreation.innerHTML += `
+              <div class="card__column">
+                  <div class="cards__body">
+                      <div class="cards__title">${form[0].value}</div>
+                      <div class="cards__email">${form[1].value}</div>
+                      <div class="cards__text">${form[2].value}</div>
+                  </div>
+              </div>
+            `
         } else {
-            alert('Заполните обязательные поля!')
-            form.classList.remove('_sending');
+            alert('Поля пустые, либо заполнены некорректно! Повторите ввод!')
         }
+        form.reset();
     }
 
+    // Функция валидации формы
     function formValidate(form) {
 
+        // Счётчик ошибок - 0 по умолчанию
         let error = 0;
+        // Слушаем поля с классом _req
         let formReq = document.querySelectorAll('._req');
 
+        // Пробегаем по массиву обязательных полей
         for (let index = 0; index < formReq.length; index++) {
+            // Собираем инпуты с обязательным заполнением 
             const input = formReq[index];
 
+            // На всякий случай убираем класс _error
             formRemoveError(input);
+            // Во имя борьмы с инъекциями экранируем спец-символы
+            input.value = input.value
+                .split('&').join('&amp;')
+                .split('<').join('&lt;')
+                .split('>').join('&gt;')
+                .split('"').join('&quot;');
 
+            // Валидация поля ввода эл.почты
             if (input.classList.contains('_email')) {
 
                 if (emailTest(input)) {
@@ -63,24 +68,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
         return error;
+
     }
 
-    async function getResponce() {
 
-        let responce = await fetch('./php/index.php');
-        let backData = await responce.json();
-        console.log(backData);
-
-        let list = document.querySelector('.comments');
-
-        list.innerHTML += `
-            <li class="comments__card">
-                <div class="comments__name">${backData.name}</div>
-                <div class="comments__email">${backData.email}</div>
-                <div class="comments__text">${backData.textarea}</div>
-            </li>
-        `
-    }
 
     function formAddError(input) {
         // Добавляем родительскому объекту класс error
