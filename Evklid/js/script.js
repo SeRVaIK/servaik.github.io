6dc48224dac542f9d3d1bd2bdc46d0b54e1de5a3
SeRVaIK <ovticalint@gmail.com>
2021-11-08 12:57:48 +0500

    11111

---
+++ Evklid/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.hero__swiper-body', {
+    loop: true,
+    pagination: {
+        el: '.hero__swiper-pagination',
+        clickable: true,
+    },
+    // autoplay: {
+    //     delay: 5000,
+    // },
+});
